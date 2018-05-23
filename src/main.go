cdc5aa67190d4ea40032074ba618cf9bac042373
Roman <mantilocal@gmail.com>
2018-05-23 19:27:50 +0300

    templates added

--- src/main.go
+++ src/main.go
@@ -1,1 +1,31 @@
-package src
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router:= gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+
+	router.GET("/", func(c *gin.Context) {
+
+		// Call the HTML method of the Context to render a template
+		c.HTML(
+			// Set the HTTP status to 200 (OK)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses (in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+
+	})
+
+	router.Run()
+}
