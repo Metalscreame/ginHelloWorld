eebf365552dd42be5a0335e0121edbad9a62518b
Roman <mantilocal@gmail.com>
2018-05-30 14:15:50 +0300

    import fast fix

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/heroku/go-getting-started/services"
-	"github.com/heroku/go-getting-started/database"
 )
 
 var router *gin.Engine
